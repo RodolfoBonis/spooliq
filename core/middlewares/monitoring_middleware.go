8c5a7040051b46ebf79112b7bf73f7b7912ced3c
D'Bonis <dev@rodolfodebonis.com.br>
2025-10-09 23:29:52 -0300

    refactor: simplify monitoring middleware by removing unnecessary logging and context handling
    - Removed redundant logging of request and response details in LogMiddleware
    - Eliminated unused imports and variables to streamline the code
    - Maintained essential request context handling for IP tracking

--- core/middlewares/monitoring_middleware.go
+++ core/middlewares/monitoring_middleware.go
@@ -2,8 +2,6 @@ package middlewares
 
 import (
 	"context"
-	"net/http"
-	"time"
 
 	"github.com/RodolfoBonis/spooliq/core/logger"
 	sentrygin "github.com/getsentry/sentry-go/gin"
@@ -31,55 +29,13 @@ func (m *MonitoringMiddleware) SentryMiddleware() gin.HandlerFunc {
 
 // LogMiddleware is a middleware for logging requests and responses.
 func (m *MonitoringMiddleware) LogMiddleware(ctx *gin.Context) {
-	start := time.Now()
 	requestID := uuid.NewString()
 	ctx.Set("requestID", requestID)
 	var responseBody = logger.HandleResponseBody(ctx.Writer)
-	var requestBody = logger.
-		HandleRequestBody(ctx.Request)
 	ctx.Writer = responseBody
 
-	// Adiciona o IP ao contexto do request
 	ctxWithIP := context.WithValue(ctx.Request.Context(), contextKey("ip"), ctx.ClientIP())
 	ctx.Request = ctx.Request.WithContext(ctxWithIP)
 
-	m.logger.Info(ctx.Request.Context(), "Requisição iniciada", logger.Fields{
-		"request_id":   requestID,
-		"ip":           ctx.ClientIP(),
-		"method":       ctx.Request.Method,
-		"url":          ctx.Request.URL.String(),
-		"user_agent":   ctx.Request.UserAgent(),
-		"request_body": requestBody,
-	})
-
 	ctx.Next()
-
-	latency := time.Since(start)
-	status := ctx.Writer.Status()
-	logFields := logger.Fields{
-		"request_id":    requestID,
-		"ip":            ctx.ClientIP(),
-		"method":        ctx.Request.Method,
-		"url":           ctx.Request.URL.String(),
-		"user_agent":    ctx.Request.UserAgent(),
-		"status":        status,
-		"latency_ms":    latency.Milliseconds(),
-		"request_body":  requestBody,
-		"response_body": responseBody.Body.String(),
-	}
-
-	if isSuccessStatusCode(status) {
-		m.logger.Info(ctx.Request.Context(), "Requisição concluída", logFields)
-	} else {
-		m.logger.Error(ctx.Request.Context(), "Requisição falhou", logFields)
-	}
-}
-
-func isSuccessStatusCode(statusCode int) bool {
-	switch statusCode {
-	case http.StatusOK, http.StatusCreated, http.StatusAccepted, http.StatusNoContent:
-		return true
-	default:
-		return false
-	}
 }
